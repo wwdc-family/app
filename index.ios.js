e2e2c920997f8be26fa9df17c239821423121f8a
Felix Krause <krausefx@gmail.com>
2017-03-31 22:02:07 -0700

    Add user registration

--- index.ios.js
+++ index.ios.js
@@ -1,53 +1,91 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
+import * as firebase from "firebase";
+
 import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  Button,
+  TextInput
 } from 'react-native';
 
+firebase.initializeApp({
+  
+});
+
 export default class wwdcfamily extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      email: 'email@krausefx.com',
+      password: 'abcdefg123',
+    }
+  }
+
+  async signup(email, pass) {
+    try {
+      console.log("start")
+      await firebase.auth().createUserWithEmailAndPassword(email, pass);
+
+      console.log("Account created");
+      // Navigate to the Home page, the user is auto logged in
+    } catch (error) {
+      console.log(error.toString())
+    }
+  }
+
+  onPressRegister = () => {
+    console.log("Pressed register")
+    console.log(this.state.email)
+    this.signup(this.state.email, this.state.password)
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <TextInput
+          style={styles.email}
+          placeholder="Email"
+          onChangeText={email => this.setState({email})}
+          value={this.state.email}
+        />
+        <TextInput
+          style={styles.password}
+          placeholder="Password"
+          secureTextEntry={true}
+          onChangeText={password => this.setState({password})}
+          value={this.state.password}
+        />
+        <Button
+          onPress={this.onPressRegister}
+          title="Register"
+          color="#841584"
+          accessibilityLabel="Signup"
+        />
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
+  email: {
     textAlign: 'center',
-    margin: 10,
+    height: 50,
+    borderStyle: 'solid',
+    borderWidth: 1,
+    margin: 30,
+    marginBottom: 0,
+    marginTop: 60
   },
-  instructions: {
+  password: {
     textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    height: 50,
+    borderStyle: 'solid',
+    borderWidth: 1,
+    margin: 30
+  }
 });
 
 AppRegistry.registerComponent('wwdcfamily', () => wwdcfamily);
