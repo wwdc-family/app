95a286863ab222a095e1d7546bc6116555970048
Felix Krause <krausefx@gmail.com>
2017-04-10 18:09:17 -0700

    Add Crashlytics

--- ios/wwdcfamily/AppDelegate.m
+++ ios/wwdcfamily/AppDelegate.m
@@ -12,10 +12,15 @@
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 
+#import <Fabric/Fabric.h>
+#import <Crashlytics/Crashlytics.h>
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [Fabric with:@[[Crashlytics class]]];
+  [Answers logCustomEventWithName:@"AppStarted" customAttributes:nil];
   NSURL *jsCodeLocation;
 
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
