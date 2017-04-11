659b9d3228596bfbc7543cb6f73570bb8cf687a3
Felix Krause <krausefx@gmail.com>
2017-04-10 20:37:44 -0700

    Setup fastlane snapshot to automate screenshots
    Fixes https://github.com/wwdc-family/app/issues/8

---
+++ ios/wwdcfamilyUITests/wwdcfamilyUITests.swift
@@ -0,0 +1,44 @@
+//
+//  wwdcfamilyUITests.swift
+//  wwdcfamilyUITests
+//
+//  Created by Felix Krause on 4/10/17.
+//  Copyright © 2017 Facebook. All rights reserved.
+//
+
+import XCTest
+
+class wwdcfamilyUITests: XCTestCase {
+  
+  override func setUp() {
+    super.setUp()
+    // In UI tests it is usually best to stop immediately when a failure occurs.
+    continueAfterFailure = false
+    // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+    let app = XCUIApplication()
+    setupSnapshot(app)
+    app.launch()
+  }
+  
+  override func tearDown() {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    super.tearDown()
+  }
+  
+  func testAppForScreenshots() {
+    let app = XCUIApplication()
+    snapshot("0_Login")
+    app.textFields["Email"].tap()
+    app.textFields["Email"].typeText("simulator@krausefx.com")
+    app.secureTextFields["Password"].tap()
+    app.secureTextFields["Password"].typeText("simulatorPassword033")
+    app.buttons["Login"].tap()
+
+    sleep(3) // to ensure the map and all markers are loaded
+    snapshot("1_Map")
+    
+    app.staticTexts["📡"].tap()
+    
+    snapshot("3_Dialog")
+  }
+}
